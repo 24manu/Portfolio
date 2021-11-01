58583274e4adf53d54e84b35a339b0964bd0648c
24manu <manuel.mouzelard@hotmail.com>
2021-11-01 13:07:06 +0100

    nouvelle structure

--- import/homepage.php
+++ import/homepage.php
@@ -1,16 +1,9 @@
 <?php
-
+include "head.php";
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-    <link type="text/css" rel="stylesheet" href="./css/style.css" />
-</head>
-<body>
+
+<main>
+
     <?php
     // on peut importer le menu en PHP sans passer par index.php
     include "menu.php";
@@ -18,16 +11,18 @@
     <h1>Accueil</h1>
 
     <p>"Choisissez un travail que vous aimez et vous n'aurez pas à travailler un seul jour de votre vie."</br>
-    - Confucius -</p>
+        - Confucius -</p>
 
     <h2>Bienvenue sur mon portfolio !</h2>
 
     <p>J'ai souhaité me réorienter dans un domaine qui me plait davantage.</br>
-    C’est pourquoi, je me suis intéressé de plus près à ce que je voulais réellement, à mes réelles passions, et j'en ai découvert une : le développement web !</br>
-    Aujourd’hui, je suis très motivé et je suis actuellement en formation de développeur web junior au centre de formation 2000.</br>
-    Vous découvrirez dans ce portfolio, mes compétences, mon parcours ainsi que les projets que j'ai pu réaliser.</br>
-    Si vous avez des questions, n'hésitez pas à me contacter.</p>
-    
-    
-</body>
-</html>
+        C’est pourquoi, je me suis intéressé de plus près à ce que je voulais réellement, à mes réelles passions, et j'en ai découvert une : le développement web !</br>
+        Aujourd’hui, je suis très motivé et je suis actuellement en formation de développeur web junior au centre de formation 2000.</br>
+        Vous découvrirez dans ce portfolio, mes compétences, mon parcours ainsi que les projets que j'ai pu réaliser.</br>
+        Si vous avez des questions, n'hésitez pas à me contacter.</p>
+
+</main>
+
+<?php
+include "footer.php";
+?>
